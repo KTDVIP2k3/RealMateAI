ea18b0f72e65808a248f2edd8839c2d0466c138b
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2026-06-02 16:20:08 +0700

    first commit - init project RealMateAi

---
+++ src/main/java/com/GSU26SE22_SU26SE002/RealMateAI/RealMateAiApplication.java
@@ -0,0 +1,13 @@
+package com.GSU26SE22_SU26SE002.RealMateAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealMateAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealMateAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GSU26SE22_SU26SE002/RealMateAI/RealMateAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GSU26SE22_SU26SE002.RealMateAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealMateAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
